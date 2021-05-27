d9f9f327d7b5c2e74f50544ba12ae470918ec10b
Peng Chen <pfc5098@gmail.com>
2021-05-27 14:06:01 -0400

    add generate_csv_in_temp_folder()

--- R/generate_csv_in_temp_folder.R
+++ R/generate_csv_in_temp_folder.R
@@ -1,3 +1,21 @@
-generate_csv_in_temp_folder <- function() {
-
-}
+#' Generate a .csv file in a temp folder
+#'
+#' Create a temp folder in your project directory
+#' and generate a csv file based on your data set of interest,
+#' which can be conveniently pasted to other reports.
+#'
+#' @param tbl the data set of interest.
+#' @param file_name name of the temp file; Default "temp".
+#'
+#' @return A .csv file in the temp folder.
+#' @export
+generate_csv_in_temp_folder <-
+  function(tbl, file_name = "temp") {
+    if(!fs::dir_exists("temp")) {
+      fs::dir_create("temp")
+      usethis::use_build_ignore("temp")
+    }
+    data.table::fwrite(
+      tbl, here::here("temp", glue::glue("{file_name}.csv"))
+    )
+  }
