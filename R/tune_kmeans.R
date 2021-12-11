0a11594853c5de2b564d72392c21e045026751a4
pfc5098 <pfc5098@gmail.com>
2021-12-11 21:40:34 +0000

    tune_kmeans

---
+++ R/tune_kmeans.R
@@ -0,0 +1,27 @@
+#' Tune K-means.
+#'
+#' @param tbl Data.
+#' @param k_max Maximum number of centers to try.
+#' @param nstart Number of random initial solutions for each trial.
+#'
+#' @return A data frame with two columns: (1) k, and (2) wss_to_tss.
+#'
+#'@importFrom rlang .data
+tune_kmeans <- function(tbl, k_max, nstart) {
+  tibble::tibble(k = 1:k_max) %>%
+    dplyr::mutate(
+      wss_to_tss = purrr::map_dbl(
+        .data$k, ~ k_to_wss_tss(tbl, k = .x, nstart = nstart)
+      )
+    )
+}
+
+# sub-function
+k_to_wss_tss <- function(tbl, k, nstart) {
+  tbl %>%
+    stats::kmeans(centers = k, nstart = nstart) %>%
+    broom::glance() %>%
+    dplyr::mutate(wss_to_tss = .data$tot.withinss / .data$totss) %>%
+    dplyr::pull(.data$wss_to_tss)
+}
+
